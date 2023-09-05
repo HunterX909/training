ccb29c660c461c7fc5b6f4e3a8d9fb8fb875ec32
JE114LB <JE114LB@IN3472849W1.mea.ey.net>
2023-09-05 15:53:31 +0530

    unit-testing project

---
+++ unit-testing/src/AssertFunctions.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class AssertFunctions {
+
+	@Test
+	public void testAssert() {
+//		String s1 = "JUnit";
+//		String s2 = "JUnit";
+//		assertEquals(s1, s2);
+//		assertSame(s1, s2);
+//		assertNotNull(s2);
+//		assertNull(s2);
+		
+		int n1 = 4;
+		int n2 = 4;
+		assertEquals(n1, n2);
+	}
+
+}
